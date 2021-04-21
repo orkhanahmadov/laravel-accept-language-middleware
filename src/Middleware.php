454655e0761f8c6ee1c633e53a9a713122987186
Orkhan Ahmadov <hi@orkhan.dev>
2021-04-21 08:11:10 +0200

    General fixes

--- src/Middleware.php
+++ src/Middleware.php
@@ -3,67 +3,50 @@
 namespace Orkhanahmadov\LaravelAcceptLanguageMiddleware;
 
 use Closure;
-use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 
 class Middleware
 {
     /**
-     * @var Application
-     */
-    private $app;
-
-    /**
-     * HttpLocaleMiddleware constructor.
-     *
-     * @param Application $app
-     */
-    public function __construct(Application $app)
-    {
-        $this->app = $app;
-    }
-
-    /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  Request  $request
      * @param  \Closure  $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        $this->app->setLocale($this->parseHttpLocale($request));
+        if ($locale = $this->parseHttpLocale($request)) {
+            app()->setLocale($locale);
+        }
 
         return $next($request);
     }
 
-    /**
-     * @param Request $request
-     *
-     * @return string
-     */
     private function parseHttpLocale(Request $request): string
     {
         $list = explode(',', $request->server('HTTP_ACCEPT_LANGUAGE'));
 
-        $locales = Collection::make($list)->map(function ($locale) {
-            $parts = explode(';', $locale);
+        $locales = Collection::make($list)
+            ->map(function ($locale) {
+                $parts = explode(';', $locale);
 
-            $mapping['locale'] = trim($parts[0]);
+                $mapping['locale'] = trim($parts[0]);
 
-            if (isset($parts[1])) {
-                $factorParts = explode('=', $parts[1]);
+                if (isset($parts[1])) {
+                    $factorParts = explode('=', $parts[1]);
 
-                $mapping['factor'] = $factorParts[1];
-            } else {
-                $mapping['factor'] = 1;
-            }
+                    $mapping['factor'] = $factorParts[1];
+                } else {
+                    $mapping['factor'] = 1;
+                }
 
-            return $mapping;
-        })->sortByDesc(function ($locale) {
-            return $locale['factor'];
-        });
+                return $mapping;
+            })
+            ->sortByDesc(function ($locale) {
+                return $locale['factor'];
+            });
 
         return $locales->first()['locale'];
     }
--- tests/MiddlewareTest.php
+++ tests/MiddlewareTest.php
@@ -3,20 +3,30 @@
 namespace Orkhanahmadov\LaravelAcceptLanguageMiddleware\Tests;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\App;
 use Orkhanahmadov\LaravelAcceptLanguageMiddleware\Middleware;
 
 class MiddlewareTest extends TestCase
 {
+    /**
+     * @var Response
+     */
+    private $response;
+    /**
+     * @var Middleware
+     */
+    private $middleware;
+
     public function test_with_single_locale()
     {
-        $middleware = $this->app->make(Middleware::class);
-        $request = Request::create('whatever', 'GET', [], [], [], [
+        $request = Request::create('/', 'GET', [], [], [], [
             'HTTP_ACCEPT_LANGUAGE' => 'es',
         ]);
         $this->assertEquals('en', App::getLocale());
 
-        $middleware->handle($request, function () {
+        $this->middleware->handle($request, function () {
+            return $this->response;
         });
 
         $this->assertEquals('es', App::getLocale());
@@ -24,13 +34,13 @@ class MiddlewareTest extends TestCase
 
     public function test_with_single_with_country()
     {
-        $middleware = $this->app->make(Middleware::class);
-        $request = Request::create('whatever', 'GET', [], [], [], [
+        $request = Request::create('/', 'GET', [], [], [], [
             'HTTP_ACCEPT_LANGUAGE' => 'es-US',
         ]);
         $this->assertEquals('en', App::getLocale());
 
-        $middleware->handle($request, function () {
+        $this->middleware->handle($request, function () {
+            return $this->response;
         });
 
         $this->assertEquals('es-US', App::getLocale());
@@ -38,13 +48,13 @@ class MiddlewareTest extends TestCase
 
     public function test_with_single_with_country_and_quality_value()
     {
-        $middleware = $this->app->make(Middleware::class);
-        $request = Request::create('whatever', 'GET', [], [], [], [
+        $request = Request::create('/', 'GET', [], [], [], [
             'HTTP_ACCEPT_LANGUAGE' => 'es-US;q=0.5',
         ]);
         $this->assertEquals('en', App::getLocale());
 
-        $middleware->handle($request, function () {
+        $this->middleware->handle($request, function () {
+            return $this->response;
         });
 
         $this->assertEquals('es-US', App::getLocale());
@@ -52,13 +62,13 @@ class MiddlewareTest extends TestCase
 
     public function test_with_multiple_locales()
     {
-        $middleware = $this->app->make(Middleware::class);
-        $request = Request::create('whatever', 'GET', [], [], [], [
+        $request = Request::create('/', 'GET', [], [], [], [
             'HTTP_ACCEPT_LANGUAGE' => 'es, de',
         ]);
         $this->assertEquals('en', App::getLocale());
 
-        $middleware->handle($request, function () {
+        $this->middleware->handle($request, function () {
+            return $this->response;
         });
 
         $this->assertEquals('es', App::getLocale());
@@ -66,13 +76,13 @@ class MiddlewareTest extends TestCase
 
     public function test_with_multiple_locales_with_countries()
     {
-        $middleware = $this->app->make(Middleware::class);
-        $request = Request::create('whatever', 'GET', [], [], [], [
+        $request = Request::create('/', 'GET', [], [], [], [
             'HTTP_ACCEPT_LANGUAGE' => 'es-US, de-DE',
         ]);
         $this->assertEquals('en', App::getLocale());
 
-        $middleware->handle($request, function () {
+        $this->middleware->handle($request, function () {
+            return $this->response;
         });
 
         $this->assertEquals('es-US', App::getLocale());
@@ -80,13 +90,13 @@ class MiddlewareTest extends TestCase
 
     public function test_with_multiple_locales_with_countries_and_quality_value()
     {
-        $middleware = $this->app->make(Middleware::class);
-        $request = Request::create('whatever', 'GET', [], [], [], [
+        $request = Request::create('/', 'GET', [], [], [], [
             'HTTP_ACCEPT_LANGUAGE' => 'es-AZ;q=0.7, de-DE;q=0.8',
         ]);
         $this->assertEquals('en', App::getLocale());
 
-        $middleware->handle($request, function () {
+        $this->middleware->handle($request, function () {
+            return $this->response;
         });
 
         $this->assertEquals('de-DE', App::getLocale());
@@ -94,15 +104,35 @@ class MiddlewareTest extends TestCase
 
     public function test_with_mixed_locale_values()
     {
-        $middleware = $this->app->make(Middleware::class);
-        $request = Request::create('whatever', 'GET', [], [], [], [
+        $request = Request::create('/', 'GET', [], [], [], [
             'HTTP_ACCEPT_LANGUAGE' => 'fr-CH, de;q=0.7, fr;q=0.9, en;q=0.8, *;q=0.5',
         ]);
         $this->assertEquals('en', App::getLocale());
 
-        $middleware->handle($request, function () {
+        $this->middleware->handle($request, function () {
+            return $this->response;
         });
 
         $this->assertEquals('fr-CH', App::getLocale());
+    }
+
+    public function test_does_not_set_app_locale_when_request_header_is_not_available()
+    {
+        $request = new Request();
+        App::setLocale('es');
+
+        $this->middleware->handle($request, function () {
+            return $this->response;
+        });
+
+        $this->assertEquals('es', App::getLocale());
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->response = new Response();
+        $this->middleware = new Middleware();
     }
 }
